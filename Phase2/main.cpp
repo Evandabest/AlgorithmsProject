5fb2e79b7c6a0457737682369fc48ee3a3d862a4
fahad-githubaction <evanhaque1@gmail.com>
2024-06-04 17:39:17 -0400

    fix: made main program

--- Phase2/main.cpp
+++ Phase2/main.cpp
@@ -1,22 +1,77 @@
-
-#include "Doubly.h"
 #include "DoublyNode.h"
-#include "DoublyNode.cpp"
-#include "Doubly.cpp"
+#include "Doubly.h"
+#include "cstdlib"
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    Doubly *doubly = new Doubly();
-    doubly->AddToTail(1);
-    doubly->AddToTail(2);
-    doubly->AddToTail(3);
-    doubly->AddToTail(4);
-    doubly->AddToTail(5);
-    doubly->PrintElements();
-    cout << endl;
-    doubly->PrintReverse();
-    cout << endl;
+
+    srand(42);
+    DoublyLList List;
+
+    for (int i = 0; i <= 14; i++)
+    {
+        List.AddToTail(rand() % 100 + 1);
+    }
+
+    cout << " - Menu: " << "\n"
+         << "1. Add a Element." << "\n"
+         << "2. Print List Elements." << "\n"
+         << "3. Print List Elements in reverese." << "\n"
+         << "4. Exit." << "\n";
+
+    int Option = 1;
+
+    cout << "Select an Option from the menu 1-4: ";
+
+    while (Option == 1 || 2 || 3 || 4)
+    {
+
+        cin >> Option;
+
+        if (Option == 1)
+        {
+
+            int newelement;
+            cout << "Option #1 is Adding a element, you choose it: ";
+            cin >> newelement;
+            if (cin.fail())
+            {
+                cin.clear();
+            }
+            else
+            {
+                List.AddToTail(newelement);
+                cout << "\n"
+                     << "Select another option from the menu: ";
+            }
+        }
+        else if (Option == 2)
+        {
+            cout << "Option #2 is List Printout: ";
+            List.PrintElements();
+            cout << "\n"
+                 << "Select another option from the menu: ";
+        }
+        else if (Option == 3)
+        {
+            cout << "Option #3 is Reverse List Printout: ";
+            List.PintReverse();
+            cout << "\n"
+                 << "Select another option from the menu: ";
+        }
+        else if (Option == 4)
+        {
+            cout << " Thanks, Hava a good Day!! " << endl;
+            break;
+        }
+        else
+        {
+            cout << "  - Invalid Input, Try again later.";
+            break;
+        }
+    }
+
     return 0;
 }
