f2c452b2217e85e57fbb0269cb5c9d50316b42fa
Edison222 <121990442+Edison222@users.noreply.github.com>
2024-06-05 15:31:10 -0400

    fix: added implementation files

--- Phase2/Doubly.h
+++ Phase2/Doubly.h
@@ -1,17 +1,51 @@
-#ifndef DOUBLY_H
-#define DOUBLY_H
-#include "DoublyNode.h"
+#include "Node.h"
 
-class Doubly
-{
-public:
-    Doubly();
-    void AddToTail(int);
-    void PrintElements();
-    void PrintReverse();
+class DoublyLList{
+ 
+ private:
+    Node* head;
+    Node* tail;
+ 
+ public:
+    DoublyLList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
 
-private:
-    DoublyNode *head;
+    void AddToTail(int n){
+
+        Node* newNode = new Node(n);
+        if (head == nullptr)
+        {
+            head = tail = newNode;
+        }else{
+            tail->next = newNode;
+            newNode->prev = tail; 
+            tail = newNode;
+        }
+    }
+
+    void PrintElements(){
+
+        Node* tempo = head;
+        while (tempo != nullptr)
+        {
+            cout<<tempo->data << " ";
+            tempo = tempo->next;
+        }
+        cout << endl;
+    }
+
+    void PintReverse(){
+
+        Node* tempo = tail;
+        while (tempo != head->prev)
+        {
+            cout << tempo->data << " ";
+            tempo = tempo->prev;
+        }
+        cout<< endl;
+        
+    }
 };
-
-#endif
---
+++ Phase2/Node.h
@@ -0,0 +1,15 @@
+class Node{
+
+ public:
+
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    };
+};
